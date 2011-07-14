756e3a6b81e537baae2f2523027956dc89825256
darianb <darianb@4832a5b8-04cd-451a-aa3d-791d7f717fd3>
2011-07-14 08:19:44 +0000

    Import test project for jira MNG-5133

---
+++ trunk/sanity4j-test/maven3-jira/example1/child2/src/test/java/muck/Hello2_Test.java
@@ -0,0 +1,13 @@
+package muck;
+
+import junit.framework.TestCase;
+
+public class Hello2_Test extends TestCase
+{
+    public void testHello2()
+    {
+        Hello2.main(null);
+        
+        assertTrue(true);
+    }
+}
